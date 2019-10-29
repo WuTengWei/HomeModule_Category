a13c4eaa5d83d9f47af4569027ba5a10c8175fa8
wutengwei <wutengwei@rocedar.com>
2019-10-29 11:40:48 +0800

    提交参数传递

--- HomeModule_Category/Classes/CTMediator+HomeModule.h
+++ HomeModule_Category/Classes/CTMediator+HomeModule.h
@@ -15,6 +15,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (UIViewController *)HomeModule_ViewController;
 
+- (UIViewController *)HomeModule_CategoryViewControllerWithCallback:(void (^)(NSString *))callback;
+
+- (UIViewController *)HomeModule_pushToDetailController:(void(^)(NSString * str))callback;
 @end
 
 NS_ASSUME_NONNULL_END
--- HomeModule_Category/Classes/CTMediator+HomeModule.m
+++ HomeModule_Category/Classes/CTMediator+HomeModule.m
@@ -12,8 +12,27 @@
 @implementation CTMediator (HomeModule)
 
 - (UIViewController *)HomeModule_ViewController {
-    return  [self performTarget:@"HomeModule" action:@"viewController" params:nil shouldCacheTarget:NO];
+    UIViewController *controller = [self performTarget:@"HomeModule" action:@"viewController" params:nil shouldCacheTarget:NO];
+    if ([controller isKindOfClass:[UIViewController class]]) {
+        // view controller 交付出去之后，可以由外界选择是push还是present
+        return controller;
+    }else {
+        // 这里处理异常场景，具体如何处理取决于产品
+        return [[UIViewController alloc] init];
+    }
 }
 
+- (UIViewController *)HomeModule_CategoryViewControllerWithCallback:(void (^)(NSString *sr))callback
+{
+    NSMutableDictionary *params = [[NSMutableDictionary alloc] init];
+    params[@"callback"] = callback;
+    return [self performTarget:@"HomeModule" action:@"Category_ViewController" params:params shouldCacheTarget:NO];
+}
+
+- (UIViewController *)HomeModule_pushToDetailController:(void(^)(NSString * str))callback {
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"callback"] = callback;
+    return [self performTarget:@"HomeModule" action:@"pushToDetailController" params:params shouldCacheTarget:NO];
+}
 
 @end
